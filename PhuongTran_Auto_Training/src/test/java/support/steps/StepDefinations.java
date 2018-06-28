6b12f2ddbe1c052a6cb3aaa7a55e4d3d2161c75a
KhoaTran <khoatran@coinhako.com>
2018-06-28 23:38:59 +0700

    Flights function

--- PhuongTran_Auto_Training/src/test/java/support/steps/StepDefinations.java
+++ PhuongTran_Auto_Training/src/test/java/support/steps/StepDefinations.java
@@ -12,26 +12,29 @@ public class StepDefinations {
 		support.openFileExcel(filePath, fileName, sheetName);
 		support.launch("firefox");
 	//	support.getElements("search-id", "lst-ib");
-		String [][] table = {
-				{"Row_Index","Column_Index"}
-				,{"0","1"}
-			};
-		support.openUrl(table);
-		String [][] table1 = {
-							{"Row_Index","Column_Index"}
-							,{"1","1"}
-		};
+//		String [][] table = {
+//				{"Row_Index","Column_Index"}
+//				,{"0","1"}
+//			};
+//		support.openUrl(table);
+//		String [][] table1 = {
+//							{"Row_Index","Column_Index"}
+//							,{"1","1"}
+//		};
 		//support.click(table1);
 
 
-		support.input(table1, "phuong tran");
-		
-		String [][] table2 = {
-				{"Row_Index","Column_Index"}
-				,{"1","2"}
-		};
-		support.click(table2);
+//		support.input(table1, "phuong tran");
+//		
+//		String [][] table2 = {
+//				{"Row_Index","Column_Index"}
+//				,{"1","2"}
+//		};
+//		support.click(table2);
 		//support.selectDropdownList("select-id", "select-demo", "Tuesday", "text");
+		
+//		 Viết function test cho Flights
+		support.getFlights();
 	}
 
 } 
